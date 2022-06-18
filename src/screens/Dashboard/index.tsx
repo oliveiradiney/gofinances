343e0cf571a2a51e9d3c4c4795e10deb32b5c8b9
oliveiradiney <dineyoliveira1999@gmail.com>
2022-06-18 17:04:48 -0300

    using styled-components and using custom fonts

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text} from 'react-native';
+import { 
+    Container, 
+    Header
+}from './styles';
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Header>
+                <Text>Diney</Text>
+            </Header>
+        </Container>
+    )
+}
---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+import theme from '../../global/styles/theme';
+
+export const Container = styled.View`
+    flex:1;
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+    width: 100%;
+    background-color: ${({theme})=> theme.colors.primary};
+`;
